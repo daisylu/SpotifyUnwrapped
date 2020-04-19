ea660a964cec1563349dac71398071b92bdf22dc
Daisy Lu <daisylu94@gmail.com>
2020-04-18 23:18:07 -0500

    hello world test

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    title, lyrics = get_song_lyrics("I can't believe I had you", "Emmit Fenn")
-    return title, lyrics
+    return "hello world"
+#     title, lyrics = get_song_lyrics("I can't believe I had you", "Emmit Fenn")
+#     return title, lyrics
 
 if __name__ == '__main__':
     app.run()
