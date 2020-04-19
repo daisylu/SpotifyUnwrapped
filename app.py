ddec87072a334ddc9d262eec0a74d79309efec0c
Daisy Lu <daisylu94@gmail.com>
2020-04-18 21:10:09 -0500

    initial code for pulling genius lyrics

---
+++ app.py
@@ -0,0 +1,12 @@
+from lyrics import get_song_lyrics
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    title, lyrics = get_song_lyrics("I can't believe I had you", "Emmit Fenn")
+    return title, lyrics
+
+if __name__ == '__main__':
+    app.run()
---
+++ lyrics.py
@@ -0,0 +1,32 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+import unicodedata
+
+def parse_genius_response(response):
+    if response["meta"]["status"] == 200:
+        first_hit = response["response"]["hits"][0]["result"]
+        full_title = unicodedata.normalize("NFKD", first_hit["full_title"])
+        lyrics_url = first_hit["url"]
+    return full_title, lyrics_url
+
+def query_genius(song_title, artist_name):
+    base_url = 'https://api.genius.com'
+    headers = {'Authorization': 'Bearer ' + os.environ["GENIUS_CLIENT_ACCESS_TOKEN"]}
+    search_url = base_url + '/search'
+    data = {'q': song_title + ' ' + artist_name}
+    response = requests.get(search_url, data=data, headers=headers)
+    return response.json()
+
+def scrap_song_url(url):
+    page = requests.get(url)
+    html = BeautifulSoup(page.text, "html.parser")
+    lyrics = html.find("div", class_="lyrics").get_text()
+    lyrics = unicodedata.normalize("NFKD", lyrics)
+    return lyrics
+
+def get_song_lyrics(song_title, artist_name):
+    response = query_genius(song_title, artist_name)
+    title, url = parse_genius_response(response)
+    lyrics_text = scrap_song_url(url)
+    return title, lyrics_text
