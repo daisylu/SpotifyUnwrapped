a76a80280d5c4717707c73ed1e2ab1572e3d1cf2
Daisy Lu <daisylu94@gmail.com>
2020-07-02 21:31:35 -0400

    refactor genius scraping into class

--- spotify_unwrapped/lyrics.py
+++ spotify_unwrapped/lyrics.py
@@ -3,33 +3,48 @@ import requests
 from bs4 import BeautifulSoup
 import unicodedata
 
-def parse_genius_response(response):
-    if response["meta"]["status"] == 200:
-        first_hit = response["response"]["hits"][0]["result"]
-        full_title = unicodedata.normalize("NFKD", first_hit["full_title"])
-        lyrics_url = first_hit["url"]
-    return full_title, lyrics_url
+class GeniusLyrics:
+    def __init__(self, token):
+        self.token = token
 
-def query_genius(song_title, artist_name):
-    base_url = 'https://api.genius.com'
-    headers = {'Authorization': 'Bearer ' + os.environ["GENIUS_CLIENT_ACCESS_TOKEN"]}
-    search_url = base_url + '/search'
-    data = {'q': song_title + ' ' + artist_name}
-    response = requests.get(search_url, data=data, headers=headers)
-    return response.json()
+    def query_genius(self, song_title, artist_name):
+        
+        base_url = "https://api.genius.com"
+        headers = {"Authorization": "Bearer " + self.token}
+        search_url = base_url + "/search"
+        data = {"q": song_title + " " + artist_name}
 
-def scrap_song_url(url):
-    print(url)
-    page = requests.get(url)
-    print(page.status_code)
-    html = BeautifulSoup(page.text, "html.parser")
-    lyrics = html.find("div", class_="lyrics").get_text()
-    print(lyrics)
-    lyrics = unicodedata.normalize("NFKD", lyrics)
-    return lyrics
+        response = requests.get(search_url, data=data, headers=headers)
 
-def get_song_lyrics(song_title, artist_name):
-    response = query_genius(song_title, artist_name)
-    title, url = parse_genius_response(response)
-    lyrics_text = scrap_song_url(url)
-    return title, lyrics_text
+        return response
+
+    def parse_genius_response(self, response):
+
+        if response.status_code == 200:
+            response = response.json()
+            first_hit = response["response"]["hits"][0]["result"]
+            full_title = unicodedata.normalize("NFKD", first_hit["full_title"])
+            lyrics_url = first_hit["url"]
+            
+        return full_title, lyrics_url
+
+    def scrap_song_url(self, url):
+
+        page = requests.get(url)
+
+        if page.status_code == 200:
+            html = BeautifulSoup(page.text, "html.parser")
+            lyrics = html.find("div", class_="lyrics").get_text()
+            lyrics = unicodedata.normalize("NFKD", lyrics)
+        else:
+            lyrics = "Not found."
+
+        return lyrics
+
+    def get_song_lyrics(self, song_title, artist_name):
+        
+        response = self.query_genius(song_title, artist_name)
+        title, url = self.parse_genius_response(response)
+        lyrics_text = self.scrap_song_url(url)
+        
+        return lyrics_text
